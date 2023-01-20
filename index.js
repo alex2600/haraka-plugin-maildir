926ab88b3e1185dbd7c6bc9ab512a8ba8d75873f
Alexander Löhr <alexander.loehr@minervis.com>
2023-01-20 20:58:20 +0100

    rewriting maildir delivery

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ const util = require("util")
 
 const copyFileP = util.promisify(fs.copyFile)
 const unlinkFileP = util.promisify(fs.unlink)
-const fileExistsP = util.promisify(fs.exists)
+const existsP = util.promisify(fs.exists)
+const linkP = util.promisify(fs.link)
 
 /////////////////////////////////////////////////////////////////////////
 
-exports.hook_queue = function (next, connection, params) {
+exports.hook_queue = function (next, connection, params) { // connection @see https://haraka.github.io/core/Connection
    const plugin = this
 
    // logging @see https://haraka.github.io/core/Logging/
    plugin.logdebug("hook queue running for plugin maildir")
+
+   // const conf = plugin.config.get('maildir.yaml')
+   // plugin.loginfo(conf)
 
    // transaction @see https://haraka.github.io/core/Transaction
    const trx = connection.transaction
@@ -40,11 +44,11 @@ exports.hook_queue = function (next, connection, params) {
       // deliver file to rcpts
       return deliverFileToRcpts(filename, tmpFile, trx.rcpt_to, plugin)
          .then(() => plugin.logdebug("done delivering all mails to maildir"))
-         .then(() => next(OK))
+         .then(() => next(OK, "delivered mail to target maildir"))
          .catch(err => handleError(err, plugin, next))
          .finally(function () {
             ws.close()
-            return tryDeleteTmpFile(tmpFile)
+            return tryDeleteFile(tmpFile)
          })
    })
 }
@@ -63,22 +67,49 @@ function deliverFileToRcpts (filename, tmpFile, rcpts, plugin) {
 
 function deliverFileToRcpt (filename, tmpFile, rcpt, plugin) {
    const {user, host, original} = rcpt
+   const {maildirBaseTemplate} = plugin.config.get('maildir.yaml')
+
    plugin.loginfo(`delivering ${filename} to ${original}`)
-   const targetFile = `./data/${user}/Maildir/tmp/${filename}` // todo
-   return fileExistsP(tmpFile)
-      .then(function (doesExist) {
-         if (!doesExist) {
-            let msg = `tmp file does not exist: "${tmpFile}"`
-            throw new Error(msg)
+   let maildirBase = maildirBaseTemplate.replaceAll("%u", user).replaceAll("%d", host)
+   const targetPath1 = `${maildirBase}/tmp`
+   const targetPath2 = `${maildirBase}/new`
+   const targetFile1 = `${targetPath1}/${filename}`
+   const targetFile2 = `${targetPath2}/${filename}`
+
+   return Promise
+      .all([existsP(tmpFile), existsP(targetPath1), existsP(targetPath2)])
+      .then(function ([tmpFileExists, targetPath1Exists, targetPath2Exists]) {
+         // check for errors
+         plugin.logdebug("checking if files and dirs exist before copying")
+         let msg = null
+         if (!tmpFileExists) {
+            msg = `tmp file does not exist: "${tmpFile}"`
          }
-         else {
-            return copyFileP(tmpFile, targetFile)
+         else if (!targetPath1Exists) {
+            msg = `path does not exist: "${targetPath1}"`
          }
+         else if (!targetPath2Exists) {
+            msg = `path does not exist: "${targetPath2}"`
+         }
+         if (msg) throw new Error(msg)
+
+         // copy mail to maildir "tmp"
+         plugin.logdebug("copy file to 'tmp' dir")
+         return copyFileP(tmpFile, targetFile1)
+            .then(function () {
+               // link to maildir "new"
+               plugin.logdebug("linking file to 'new' dir")
+               return linkP(targetFile1, targetFile2)
+            })
+            .then(function () {
+               plugin.logdebug("delete file in 'tmp' dir")
+               return tryDeleteFile(targetFile1)
+            })
       })
 }
 
-async function tryDeleteTmpFile (tmpFile) {
-   if (await fileExistsP(tmpFile)) {
-      return unlinkFileP(tmpFile)
+async function tryDeleteFile (file) {
+   if (await existsP(file)) {
+      return unlinkFileP(file)
    }
 }
