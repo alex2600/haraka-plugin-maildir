6b906a3f604fcbccc6ed0731f7a56ba8e8821dc8
Alexander Löhr <alexander.loehr@minervis.com>
2023-01-20 20:15:04 +0100

    rewriting plugin

--- index.js
+++ index.js
@@ -1,218 +1,82 @@
-const os = require('os')
-const fs = require('fs')
-const path = require('path')
-const mkdirp = require('mkdirp')
-const util = require('util')
+const os = require("os")
+const fs = require("fs")
+const path = require("path")
+const util = require("util")
 
 /////////////////////////////////////////////////////////////////////////
 
-module.exports = {
-   hook_queue,
-}
+const copyFileP = util.promisify(fs.copyFile)
+const unlinkFileP = util.promisify(fs.unlink)
+const fileExistsP = util.promisify(fs.exists)
 
-/**
- * Saves email when the smtp server enqueues it.
- */
-function hook_queue (next, connection) {
-   const accept = function () {
-      next(OK)
-   }
+/////////////////////////////////////////////////////////////////////////
 
+exports.hook_queue = function (next, connection, params) {
    const plugin = this
-   const t = connection.transaction
-   const cfg = this.config.get('maildir.ini', 'ini')
 
-   const mail_from = extractEmail(t.mail_from.original)
-   const rcpt_to = t.rcpt_to.map(function (to) {
-      return to.user + '@' + to.host
-   })
+   // logging @see https://haraka.github.io/core/Logging/
+   plugin.logdebug("hook queue running for plugin maildir")
 
-   const maildir = new Maildir(cfg.main, this, connection)
-   const stream = t.message_stream
+   // transaction @see https://haraka.github.io/core/Transaction
+   const trx = connection.transaction
+   const messageStream = trx.message_stream
 
-   // Give the possibility to force the maildir user.
-   // const forced = trim(t.header.get('x-maildir-force-user')) || cfg.main.force_user
-   const forced = cfg.main.force_user // don't allow header overwrite
-   if (forced) {
-      maildir.maildir({user: forced}).messageStream(stream, accept)
-      return
-   }
+   // create mailbox filename
+   const filename = `${new Date().valueOf()}.${trx.uuid}.${connection.local.host}`
 
-   // Collect mailboxes.
-   const mailboxes = []
+   // create temporary filePath
+   const tmpDir = os.tmpdir()
+   const tmpFile = path.join(tmpDir, filename)
+   plugin.logdebug(`tmp file is ${tmpFile}`)
 
-   // Sender could be empty ("<>" value) in some bounce messages.
-   // Thanks Koshroy.
-   if (mail_from) {
-      // Populate ".Sent" dir of sender.
-      mailboxes.push({
-         user: mail_from,
-         folder: '.Sent',
-      })
-   }
-   else {
-      this.logdebug('Mail from is empty, probably this is a bounce message.')
-   }
+   // write to tmp file
+   const ws = fs.createWriteStream(tmpFile)
+   messageStream.pipe(ws)
 
-   rcpt_to.forEach(function (user) {
-      mailboxes.push({user})
-   });
+   ws.on("error", (err) => plugin.logerror(err.toString()))
+   ws.on("finish", function () {
+      plugin.logdebug("ws.finish: mail written to tmpFile")
 
-   // Process mailboxes.
-   (function nextMailbox (i, cb) {
-      if (i === mailboxes.length) {
-         cb()
-      }
-      else {
-         maildir.maildir(mailboxes[i]).messageStream(stream, function () {
-            nextMailbox(i + 1, cb)
+      // deliver file to rcpts
+      return deliverFileToRcpts(filename, tmpFile, trx.rcpt_to, plugin)
+         .then(() => plugin.logdebug("done delivering all mails to maildir"))
+         .then(() => next(OK))
+         .catch(function (err) {
+            plugin.logerror(err.toString())
+            // DENY if mail could not be delivered
+            return next(DENY, err.toString())
          })
-      }
-   }(0, accept))
+         .finally(function () {
+            ws.close()
+            return tryDeleteTmpFile(tmpFile)
+         })
+   })
 }
 
 /////////////////////////////////////////////////////////////////////////
 
-/**
- * Object for managing maildirs.
- * @param {hash} cfg
- */
-class Maildir {
-   constructor (cfg, plugin, conn) {
-      this.cfg = cfg
-      this.plugin = plugin
-      this.connection = conn
-   }
+function deliverFileToRcpts (filename, tmpFile, rcpts, plugin) {
+   return Promise.all(rcpts.map(rcpt => deliverFileToRcpt(filename, tmpFile, rcpt, plugin)))
+}
 
-   /**
-    * Unique name of the file inside the maildir.
-    * Thanks: http://cr.yp.to/proto/maildir.html
-    *
-    * @return {string}
-    */
-   filename () {
-      // For filename uniqueness, connection uuid is used.
-      const uuid = this.connection.uuid
-      const d = new Date()
-      const name = d.valueOf() + '.' + uuid + '.' + os.hostname()
-      return name
-   }
-
-   maildir (params) {
-      const self = this
-      const user = params.user
-      const folder = params.folder
-      const userParts = user.split('@')
-      const name = userParts[0], domain = userParts[1]
-      let dirMode, fileMode
-
-      return {
-         ready: function (callback) {
-            const fileName = self.fileName()
-            const dirs = ['tmp', 'cur', 'new']
-            let maildir = self.cfg.path
-            dirMode = parseInt(self.cfg.dir_mode, 8)
-            fileMode = parseInt(self.cfg.file_mode, 8)
-
-            const replace = {d: domain, n: name}
-            for (let v in replace) {
-               maildir = maildir.replace('%' + v, replace[v])
-            }
-
-            // Checks if maildir location is relative to haraka.
-            if (path.sep !== maildir.charAt(0)) {
-               maildir = path.join(process.env.HARAKA, maildir) // TODO check if env.HARAKA is set
-            }
-
-            const f = {}
-            dirs.forEach(function (dir) {
-               const parts = [maildir]
-               if (folder) {
-                  parts.push(folder)
-               }
-               parts.push(dir)
-               parts.push(fileName)
-               f[dir] = path.join.apply(path, parts)
-            });
-
-            (function nextDir (i, cb) {
-               if (i === dirs.length) {
-                  return cb()
-               }
-
-               var dir = path.dirname(f[dirs[i]])
-               fs.exists(dir, function (exists) {
-                  if (exists) {
-                     nextDir(i + 1, cb)
-                  }
-                  else {
-                     mkdirp(dir, dirMode, function (err) {
-                        if (err) {
-                           throw err
-                        }
-                        nextDir(i + 1, cb)
-                     })
-                  }
-               })
-            }(0, function () {
-               self.plugin.logdebug('Maildir ready: ' + util.format('%j, %s', f, fileName))
-               callback(f, fileName)
-            }))
-         },
-         messageStream: function (stream, callback) {
-            this.ready(function (f, name) {
-               var fileStream = fs.createWriteStream(f['tmp'], {
-                  flags: 'w',
-                  mode: fileMode
-               })
-               stream.pipe(fileStream)
-               fileStream.on('finish', function () {
-                  fs.link(f['tmp'], f['new'], function (err) {
-                     if (err) {
-                        throw err
-                     }
-                     fs.unlink(f['tmp'], function (err) {
-                        if (err) {
-                           throw err
-                        }
-                        callback()
-                     })
-                  })
-               })
-            })
+function deliverFileToRcpt (filename, tmpFile, rcpt, plugin) {
+   const {user, host, original} = rcpt
+   plugin.loginfo(`delivering ${filename} to ${original}`)
+   const targetFile = `./data/${user}/Maildir/tmp/${filename}` // todo
+   return fileExistsP(tmpFile)
+      .then(function (doesExist) {
+         if (!doesExist) {
+            let msg = `tmp file does not exist: "${tmpFile}"`
+            throw new Error(msg)
          }
-      }
-   }
+         else {
+            return copyFileP(tmpFile, targetFile)
+         }
+      })
 }
 
-/////////////////////////////////////////////////////////////////////////
-
-/**
- * Extracts the email from a recipient address (USER <EMAIL>).
- * See "Address Specification" in http://tools.ietf.org/html/rfc2822.
- *
- * @param  {string} rcpt the recipient
- * @return {string}      the extracted email.
- */
-function extractEmail (rcpt) {
-   var emailRegexp = /([^<@\s,]+@[^@>,\s]+)/
-   var match
-
-   if ((match = rcpt.match(emailRegexp)) && match.length) {
-      return match[1]
+async function tryDeleteTmpFile (tmpFile) {
+   if (await fileExistsP(tmpFile)) {
+      return unlinkFileP(tmpFile)
    }
-
-   return null
-}
-
-
-/**
- * Removes both leading and trailing whitespaces
- * from a string.
- *
- * @param  {string} string
- * @return {string}
- */
-function trim (string) {
-   return string.replace(/^\s+|\s+$/g, '')
 }
